e37be6be2aa07903112430a6af50216b4713a92b
eriik1 <83531682+eriik1@users.noreply.github.com>
2021-09-22 14:26:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup(){
+    canvas=createCanvas(500,500)
+    canvas.center()
+    video=createCapture(VIDEO)
+    video.hide()
+}
+song=""
+function preload(){
+    song=loadSound("music.mp3")
+}
+function play(){
+    song.play()
+}
+function draw(){
+    image(video,0,0,500,500)
+}
